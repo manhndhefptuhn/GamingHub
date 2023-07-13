422d7ab500817d5759ca77f5b981e24ddf95cbf6
manhndhefptuhn <manhndhe170523@fpt.edu.vn>
2023-07-13 14:05:26 +0700

    Update feedback for logic

--- src/java/Controller/Support/GetFeedbackDetails.java
+++ src/java/Controller/Support/GetFeedbackDetails.java
@@ -3,9 +3,11 @@ package Controller.Support;
 import DAL.FeedbackDAO;
 import DAL.UserDAO;
 import DAL.FeedbackResponseDAO;
+import DAL.ProductDAO;
 import Model.Feedback;
 import Model.User;
 import Model.FeedbackResponse;
+import Model.Product;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,46 +15,41 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author admin
  */
-@WebServlet(name="feedbackDetail", urlPatterns={"/feedbackDetail"})
+@WebServlet(name = "feedbackDetail", urlPatterns = {"/feedbackDetail"})
 public class GetFeedbackDetails extends HttpServlet {
-   
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         FeedbackDAO fbDAO = new FeedbackDAO();
         UserDAO uDAO = new UserDAO();
+        ProductDAO pDAO = new ProductDAO();
         FeedbackResponseDAO fbrDAO = new FeedbackResponseDAO();
-        
         //get feedack ID from feedback list
         String id = request.getParameter("id");
-        
-        
-        Feedback x = fbDAO.getFeedbackDetails(id);  
-        
+        Feedback x = fbDAO.getFeedbackDetails(id);
         User u = uDAO.getUserByID(x.getUserId());
-        
+        Product p = pDAO.getProductByID(x.getProductId());
         
         ArrayList<FeedbackResponse> fbrlst = fbrDAO.getFeedbackReplyWithId(id);
-        ArrayList<Feedback> lst = fbDAO.getFeedbackWithId(id);
-        
+
+        request.setAttribute("p", p);
         request.setAttribute("userDetails", u);
-        request.setAttribute("details", lst);
+        request.setAttribute("feedbackDetail", x);
         request.setAttribute("fbrlst", fbrlst);
         request.getRequestDispatcher("SupportFeedbackDetail.jsp").forward(request, response);
 
-    
-   
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -60,12 +57,13 @@ public class GetFeedbackDetails extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -73,12 +71,13 @@ public class GetFeedbackDetails extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
